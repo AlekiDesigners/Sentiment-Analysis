827b6e6511d9df3a925d9f028ace856f4aa04427
Aleki <root@ZJCLOE>
2024-02-22 04:36:30 +0300

    changes

--- server.py
+++ server.py
@@ -6,10 +6,13 @@ app = Flask("Sentiment Analyzer")
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
     text_to_analyse = request.args.get('textToAnalyze')
-    response = sentiment_analyzer(text_to_analyze)
+    response = sentiment_analyzer(text_to_analyse)
     label = response["label"]
     score = response["score"]
-    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+    if label is None:
+        return "Invalid input ! Try again."
+    els:
+        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
 
 @app.route("/")
 def render_index_page():
